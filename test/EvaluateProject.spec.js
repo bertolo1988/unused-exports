e1bd9497b6a42a8bf9b86e38ce18c2b93984d507
Tiago Bértolo <tiagobertolo@gmail.com>
2018-06-10 01:31:41 +0100

    Complete tests on getAllUnusedExports.

--- test/EvaluateProject.spec.js
+++ test/EvaluateProject.spec.js
@@ -6,7 +6,7 @@ const EvaluateProject = require('../src/EvaluateProject')
 const projectPath = path.join(__dirname, '../sample-project')
 
 describe('EvaluateProject', function() {
-    describe.only('getModuleUnusedExports', () => {
+    describe('getModuleUnusedExports', () => {
         it('should successfully retrieve all unused exports on ModuleExample2', () => {
             let modulePath = path.join(__dirname, '../sample-project/folder1/ModuleExample2')
             let unusedExports = EvaluateProject.getModuleUnusedExports(projectPath, modulePath)
@@ -46,6 +46,32 @@ describe('EvaluateProject', function() {
                 { file: path.join(__dirname, '../sample-project/ModuleExample1.js'), unusedExports: ['jjj'] },
                 { file: path.join(__dirname, '../sample-project/UnusedModule1.js'), unusedExports: ['lmao', 'aaa'] },
                 { file: path.join(__dirname, '../sample-project/folder1/ModuleExample2.js'), unusedExports: ['ee'] },
+                { file: path.join(__dirname, '../sample-project/folder1/folder2/UnusedModule2.js'), unusedExports: ['lol'] }]
+            unusedExports.should.be.eql(expectedResult)
+        })
+
+        it('should retrieve all unused exports if we ignore folder1', () => {
+            let ignore = path.join(__dirname, '../sample-project/folder1')
+            let unusedExports = EvaluateProject.getAllUnusedExports(projectPath, ignore)
+            const expectedResult = [
+                { file: path.join(__dirname, '../sample-project/ModuleExample1.js'), unusedExports: ['bb', 'aa', 'jjj'] },
+                { file: path.join(__dirname, '../sample-project/UnusedModule1.js'), unusedExports: ['lmao', 'aaa'] }]
+            unusedExports.should.be.eql(expectedResult)
+        })
+
+        it('should retrieve all unused exports if we ignore the whole project', () => {
+            let ignore = path.join(__dirname, '../sample-project')
+            let unusedExports = EvaluateProject.getAllUnusedExports(projectPath, ignore)
+            unusedExports.should.be.eql([])
+        })
+
+        it('should retrieve all unused exports if we ignore ModuleExample4', () => {
+            let ignore = path.join(__dirname, '../sample-project/folder1/folder2/ModuleExample4.js')
+            let unusedExports = EvaluateProject.getAllUnusedExports(projectPath, ignore)
+            const expectedResult = [
+                { file: path.join(__dirname, '../sample-project/ModuleExample1.js'), unusedExports: ['jjj'] },
+                { file: path.join(__dirname, '../sample-project/UnusedModule1.js'), unusedExports: ['lmao', 'aaa'] },
+                { file: path.join(__dirname, '../sample-project/folder1/ModuleExample2.js'), unusedExports: ['dd', 'ee'] },
                 { file: path.join(__dirname, '../sample-project/folder1/folder2/UnusedModule2.js'), unusedExports: ['lol'] }]
             unusedExports.should.be.eql(expectedResult)
         })
